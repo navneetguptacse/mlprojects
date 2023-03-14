8ad1d5b940964645dd8504acd5d3e6e280e44cfb
navneetguptacse <navneetguptaid7@gmail.com>
2023-03-14 08:47:12 +0530

    next-commit

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,11 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_messages_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frmae.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
-
     )
     return error_message
 
@@ -19,3 +18,9 @@ class CustomeException(Exception):
     def __str__(self):
         return self.error_message
 
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Devision by Zero")
+#         raise CustomeException(e,sys)
